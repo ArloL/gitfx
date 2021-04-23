6019dbac42cf55f492e5037725a061fe70cb8ae4
Arlo O'Keeffe <tiger@k5d.de>
2021-04-24 00:35:27 +0200

    get git info asynchronously

--- src/main/java/gitfx/GitFX.java
+++ src/main/java/gitfx/GitFX.java
@@ -1,7 +1,12 @@
 package gitfx;
 
+import static java.util.stream.Collectors.joining;
+
 import java.io.File;
 import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.stream.Stream;
 
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.ObjectId;
@@ -9,60 +14,69 @@ import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
-import org.eclipse.jgit.treewalk.TreeWalk;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class GitFX extends Application {
 
+	ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(1);
+
+	Repository repository;
+
 	@SuppressFBWarnings(value = "BC_UNCONFIRMED_CAST_OF_RETURN_VALUE", justification = "FileRepositoryBuilder uses generics which spotbugs cant know")
-	private static String jgit() throws IOException {
-		try (Repository repository = new FileRepositoryBuilder()
-				.setMustExist(true)
+	private Stream<RevCommit> jgit() throws IOException {
+		repository = new FileRepositoryBuilder().setMustExist(true)
 				.readEnvironment()
 				.findGitDir(new File("/Users/aokeeffe/.dotfiles"))
 				.build();
-				RevWalk revWalk = new RevWalk(repository);
-				TreeWalk treeWalk = new TreeWalk(repository)) {
-			ObjectId headId = repository.resolve(Constants.HEAD);
-			RevCommit headCommit = revWalk.parseCommit(headId);
-			return headCommit.getShortMessage();
-		}
+		RevWalk revWalk = new RevWalk(repository);
+		ObjectId headId = repository.resolve(Constants.HEAD);
+		RevCommit headCommit = revWalk.parseCommit(headId);
+		return Stream.iterate(headCommit, c -> c.getParentCount() > 0, c -> {
+			try {
+				return revWalk.parseCommit(c.getParent(0));
+			} catch (IOException e) {
+				throw new UncheckedIOException(e);
+			}
+		}).onClose(revWalk::close);
 	}
 
 	@Override
 	public void start(Stage stage) {
-		String javaVersion = System.getProperty("java.version");
-		String javafxVersion = System.getProperty("javafx.version");
-		Label label = new Label("Hello, JavaFX " + javafxVersion
-				+ ", running on Java " + javaVersion + ".");
-		Label git = new Label("");
-		try {
-			git = new Label(jgit());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		Label git = new Label("...");
 
-		ImageView imageView = new ImageView(new Image(
-				GitFX.class.getResourceAsStream("/gitfx/openduke.png")));
-		imageView.setFitHeight(200);
-		imageView.setPreserveRatio(true);
-
-		VBox root = new VBox(30, imageView, label, git);
+		VBox root = new VBox(30, git);
 		root.setAlignment(Pos.CENTER);
 		Scene scene = new Scene(root, 640, 480);
-		scene.getStylesheets()
-				.add(GitFX.class.getResource("styles.css").toExternalForm());
 		stage.setScene(scene);
 		stage.show();
+
+		executor.execute(() -> {
+			try {
+				String jgit = jgit().limit(10)
+						.map(RevCommit::getShortMessage)
+						.collect(joining("\n"));
+				Platform.runLater(() -> git.setText(jgit));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
+	}
+
+	@Override
+	public void stop() throws Exception {
+		executor.shutdown();
+		if (repository != null) {
+			repository.close();
+		}
+		super.stop();
 	}
 
 	public static void main(String[] args) {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,8 @@ module gitfx {
 	requires transitive javafx.controls;
 	requires org.eclipse.jgit;
 	requires com.github.spotbugs.annotations;
+	requires javafx.graphics;
+	requires javafx.base;
 
 	exports gitfx;
 
