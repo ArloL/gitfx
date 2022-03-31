90c4032fac3e7b2c91e3529a2d45bfaf0ce2989c
Arlo O'Keeffe <a.okeeffe@sprinteins.com>
2022-03-31 22:36:21 +0200

    update gitfx.sh

--- src/main/packaging/macos/bin/gitfx.sh
+++ src/main/packaging/macos/bin/gitfx.sh
@@ -4,6 +4,21 @@ set -o errexit
 set -o nounset
 #set -o xtrace
 
-realpath() { python -c "import os,sys; print(os.path.realpath(sys.argv[1]))" "$0"; }
-CONTENTS="$(dirname "$(dirname "$(dirname "$(dirname "$(realpath "$0")")")")")"
-open "${CONTENTS}" --args "$@"
+app_realpath() {
+	SOURCE=$1
+	while [ -h "$SOURCE" ]; do
+		DIR=$(dirname "$SOURCE")
+		SOURCE=$(readlink "$SOURCE")
+		[ "${SOURCE}" = "${SOURCE#/}" ] && SOURCE=$DIR/$SOURCE
+	done
+	SOURCE_DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+	echo "${SOURCE_DIR%%"${SOURCE_DIR#*.app}"}"
+}
+
+APP_PATH="$(app_realpath "${0}")"
+if [ -z "$APP_PATH" ]; then
+	echo "Unable to determine app path from symlink : ${0}"
+	exit 1
+fi
+open "${APP_PATH}" --args "$(readlink -f "${1}")"
+exit $?
